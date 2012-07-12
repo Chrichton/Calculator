5dcd6508d8e1e705b6cd23f768c7d7570b69ad9b
Heiko Goes <heiko.goes@gaebler-goes.de>
2012-07-12 21:14:55 +0200

    GraphicController

---
+++ Calculator/GraphicsXYViewController.m
@@ -0,0 +1,44 @@
+//
+//  GraphicsXYViewController.m
+//  Calculator
+//
+//  Created by Heiko Goes on 12.07.12.
+//  Copyright (c) 2012 my Software Goes. All rights reserved.
+//
+
+#import "GraphicsXYViewController.h"
+
+@interface GraphicsXYViewController ()
+@property (nonatomic) NSDictionary *data;
+@end
+
+@implementation GraphicsXYViewController
+@synthesize data = _data;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
